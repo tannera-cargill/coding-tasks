b5355879591d86823c3135c5dd7c5ec2aea2fbb2
tannera-cargill <107029512+tannera-cargill@users.noreply.github.com>
2023-05-04 13:17:19 +0200

    Update task3-promises.ts

--- task3-promises/task3-promises.ts
+++ task3-promises/task3-promises.ts
@@ -16,52 +16,9 @@ asyncFunction();
  * whilst keeping exactly the same behaviour.
  * */
 
+
 /**
  * Part 2:
- * Lets refactor the below snippet to remove the onFinished callback from taskA() and use
- * promises instead.
- * */
-
-// refactor this
-function taskA(onFinished: (token: string) => string) {
-  const getToken = () => Promise.resolve("abc");
-  return getToken().then((value) => {
-    return onFinished(value);
-  });
-}
-
-// leave taskB untouched.
-function taskB(token: string) {
-  return token;
-}
-
-// refactor this
-export const tokenGetter = () => {
-  return taskA((value) => taskB(value));
-};
-
-/**
- * Part 3:
- * Refactor the below code to run taskA() and taskB() concurrently in the executor.
- * */
-
-function callA() {
-  return Promise.resolve("resultA");
-}
-
-function callB() {
-  return Promise.resolve("resultB");
-}
-
-const executorPart3 = async () => {
-  const resultA = await callA();
-  const resultB = await callB();
-
-  return [resultA, resultB];
-};
-
-/**
- * Part 4:
  * Lets refactor tokenGetterFunc() to make it more reusable by allowing us to provide a different getToken() function.
  * */
 async function tokenGetterFunc() {
