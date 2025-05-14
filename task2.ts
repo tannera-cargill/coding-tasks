4fe64d71f8d4caa7bcb8c3c0387207a831b8b862
tannera-cargill <107029512+tannera-cargill@users.noreply.github.com>
2025-05-14 18:35:08 +0200

    update ts version

--- task2.ts
+++ task2.ts
@@ -15,18 +15,3 @@ function processA(text: string | null): string | null {
 
   return text.toUpperCase();
 }
-
-/**
- * TypeScript Generics (advanced)
- *
- * Let's implement a simple useQuery(callback: (...args): any[] => any) hook which takes a callback as parameter, and returns a 'query()' function which
- * infers the same function signature as the input callback, but only takes the first parameter, and the return type of query() is always void.
- * @example
- * const { query } = useQuery((param: string) => Promise.resolve())
- *
- * // OK
- * query('someString')
- *
- * // TS Error: Argument of type 'number' is not assignable to parameter of type 'string'.
- * query(1)
- * */
